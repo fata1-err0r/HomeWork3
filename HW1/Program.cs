407d1506791f85d18ccb88ffe49c8c89d3187fc5
fata1-err0r <viper_e398@mail.ru>
2021-03-31 10:49:36 +0700

    some fixes

--- HW1/Program.cs
+++ HW1/Program.cs
@@ -1,96 +1,57 @@
 ﻿using System;
 using System.Collections.Generic;
 
-namespace HW1
+namespace HomeWork3._1
 {
-	class Program
-	{
-		static void Main(string[] args)
-		{
-			do
-			{
-				try
-				{
-					Console.Clear();
-					Console.WriteLine("Введите время работы почты: ");
-					int time = Convert.ToInt32(Console.ReadLine());
-					if (time > 0)
-					{
-					VisitorsN:
-						Console.WriteLine("Введите кол-во посетителей: ");
-						int visitorsN = Convert.ToInt32(Console.ReadLine());
-						if (visitorsN > 0)
-						{
-							Queue<int> visitorsT = new Queue<int>();
-							for (int i = 0; i < visitorsN; i++)
-							{
-								Console.WriteLine($"Введите время обслуживания {i+1} посетителя: ");
-								int a = Convert.ToInt32(Console.ReadLine());
-								if (a > 0)
-								{
-									visitorsT.Enqueue(a);
-								}
-								else
-								{
-									Console.WriteLine("Время обслуживания должно быть больше 0! ");
-									i--;
-								}
-							}
-							do
-							{
-								time = time - visitorsT.Dequeue();
-								if (visitorsT.Count == 0)
-								{
-									break;
-								}
-							}
-							while (time >= 0);
-							switch (visitorsT.Count)
-							{
-								case 0:
-									Console.WriteLine("Почта успела обслужить всех! ");
-									Console.WriteLine("Для продолжения нажмите любую клавишу... ");
-									Console.ReadKey();
-									break;
-								default:
-									Console.WriteLine($"Почта не успела обслужить {visitorsT.Count} посетителей ");
-									Console.WriteLine("Для продолжения нажмите любую клавишу... ");
-									Console.ReadKey();
-									break;
-							}
-						}
-						switch (visitorsN)
-						{
-							case 0:
-								Console.WriteLine("Сегодня на почту никто не пришел.. ");
-								goto VisitorsN;
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите время работы почты: ");
+            int postT = CheckInt();
+            Console.Write("Введите количество посетителей: ");
+            int visitorsN = CheckInt();
+            Queue<int> time = new Queue<int>();
+            Console.WriteLine("Введите время обслуживания посетителя: ");
+            for (int i = 1; i <= visitorsN; i++)
+            {
+                Console.Write($"На {i} посетителя будет затрачено: ");
+                time.Enqueue(CheckInt());
+            }
+            int visitorsS = 0;
+            while ((postT > 0) && (time.Count != 0))
+            {
+                postT -= time.Dequeue();
+                visitorsS++;
+            }
+            Console.Write($"Почта не успела обслужить {visitorsN - visitorsS} посетителей ");
+            Console.ReadKey();
+        }
+        static int CheckInt() // Метод для проверки целочисленного положительного числа
+        {
 
-							case < 0:
-								Console.WriteLine("Сегодня на почте произошло что-то странное.. ");
-								goto VisitorsN;
-						}
-					}
-					else if (time == 0)
-					{
-						Console.WriteLine("Сегодня почта не работает.. ");
-						Console.WriteLine("Для продолжения нажмите любую клавишу... ");
-						Console.ReadKey();
-					}
-					else
-					{
-						Console.WriteLine("Время не может быть отрицательным! ");
-						Console.WriteLine("Для продолжения нажмите любую клавишу... ");
-						Console.ReadKey();
-					}
-				}
-				catch
-				{
-					Console.WriteLine("ERROR введено не верное значение! ");
-					Console.WriteLine("Для продолжения нажмите любую клавишу... ");
-					Console.ReadKey();
-				}
-			}
-			while (true);
-		}
-	}
+            int n;
+            while (true)
+            {
+                string input = Console.ReadLine();
+                bool check = int.TryParse(input, out n);
+                if (check)
+                {
+                    if (n <= 0)
+                    {
+                        Console.WriteLine("ERROR введено не верное значение! ");
+                        Console.Write("Введите значение: ");
+                    }
+                    else
+                        break;
+                }
+                else
+                {
+                    Console.WriteLine("ERROR введено не верное значение! ");
+                    Console.Write("Введите значение: ");
+                }
+            }
+            return n;
+        }
+    }
 }
